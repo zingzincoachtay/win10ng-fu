73cf1c7f9bde09d14af2206e2fc35b027ee88a07
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-08-21 14:27:33 -0400

    Redacted for publish

--- side-by-side.bat
+++ side-by-side.bat
@@ -4,10 +4,11 @@ echo[
 echo Highly recommend maximizing the prompt
 echo Set at 200 print columns
 echo Utilize Diffutils (Win) package
+set diff=C:\Users\kaoki\Documents\coreutils\bin\diff.exe
 
 echo[
 echo Compare two indices and show differences side by side
-C:\Users\kaoki\Documents\coreutils\bin\diff.exe -iEb --side-by-side --suppress-common-lines -W 200 --minimal "%~1" "%~2"
+%diff% -iEb --side-by-side --suppress-common-lines -W 200 --minimal "%~1" "%~2"
 
 
 echo[
