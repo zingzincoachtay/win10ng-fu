ca71ec1b7c7661ba16b652016ade11c949fff985
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-08-21 14:28:31 -0400

    Redacted
    Redacted for publishing

--- Tracking Projects.bat
+++ Tracking Projects.bat
@@ -14,14 +14,14 @@ echo Use either the md5deep package or md5sum package
 set md5=C:\Users\kaoki\Documents\md5deep-4.4\md5deep64.exe
 echo[
 echo Set directories to check
-set here[1]=
-set here[2]=
-set here[3]=
+set here[1]="H:PURCHASING\02 QUOTES\01 MATERIAL & COMPONENT Suppliers"
+set here[2]="H:PURCHASING\02 QUOTES\02 MOLDED PART Suppliers"
+set here[3]="H:\PURCHASING\08 COST REDUCTION STUFF"
 echo[
 echo Set output files
-set this[1]=
-set this[2]=
-set this[3]=
+set this[1]="C:\Users\kaoki\Desktop\02quotes01 %today%.sum"
+set this[2]="C:\Users\kaoki\Desktop\02quotes02 %today%.sum"
+set this[3]="C:\Users\kaoki\Desktop\08cost reduction %today%.sum"
 
 for /l %%k in (1,1,3) do (
   call :hashfile %%this[%%k]%% %%here[%%k]%%
