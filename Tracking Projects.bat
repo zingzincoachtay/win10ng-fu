b0b9af858911987414055ca38498b6e1a1bb8ec6
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-08-21 13:31:29 -0400

    Be less repetitive

--- Tracking Projects.bat
+++ Tracking Projects.bat
@@ -1,42 +1,49 @@
 @echo off
+echo[
 echo The day starts with checking for changes in the project folders.
 echo The data files may be updated or new files may have been added yesterday.
-echo[
 
 rem https://oscarliang.com/change-date-ouput-format-windows-batch-script/
-
-
-
-set today=%Date:~10,4%-%Date:~4,2%-%Date:~7,2%(%Date:~0,3%)%Time:~0,2%-%Time:~3,2%-%Time:~6,2%
-
+echo[
+rem set today=%Date:~10,4%-%Date:~4,2%-%Date:~7,2%(%Date:~0,3%)%Time:~0,2%-%Time:~3,2%-%Time:~6,2%
 set today=%Date:~10,4%-%Date:~4,2%-%Date:~7,2%(%Date:~0,3%)
+echo Today's Timestamp %today%
 
 echo[
-set Qone="C:\Users\kaoki\Desktop\02quotes01 %today%.sum"
-if not exist %Qone% (
-echo Digesting all quotes in "MATERIAL & COMPONENT Suppliers"
-C:\Users\kaoki\Documents\md5deep-4.4\md5deep64.exe -r "H:PURCHASING\02 QUOTES\01 MATERIAL & COMPONENT Suppliers" | sort /+35 > %Qone%
-echo ...Done
-)
+echo Use either the md5deep package or md5sum package
+set md5=C:\Users\kaoki\Documents\md5deep-4.4\md5deep64.exe
 echo[
-set Qtwo="C:\Users\kaoki\Desktop\02quotes02 %today%.sum"
-if not exist %Qtwo% (
-echo Digesting all quotes in "MOLDED PART Suppliers"
-C:\Users\kaoki\Documents\md5deep-4.4\md5deep64.exe -r "H:PURCHASING\02 QUOTES\02 MOLDED PART Suppliers" | sort /+35 > %Qtwo%
-echo ...Done
+echo Set directories to check
+set here[1]="H:PURCHASING\02 QUOTES\01 MATERIAL & COMPONENT Suppliers"
+set here[2]="H:PURCHASING\02 QUOTES\02 MOLDED PART Suppliers"
+echo[
+echo Set output files
+set this[1]="C:\Users\kaoki\Desktop\02quotes01 %today%.sum"
+set this[2]="C:\Users\kaoki\Desktop\02quotes02 %today%.sum"
+
+for /l %%k in (1,1,2) do (
+  call :hashfile %%this[%%k]%% %%here[%%k]%%
 )
 
 echo[
 set CR="C:\Users\kaoki\Desktop\08cost reduction %today%.sum"
 if not exist %CR% (
-echo Moving along.. Digesting any updates to the cost reduction proposal estimates.
-C:\Users\kaoki\Documents\md5deep-4.4\md5deep64.exe  "H:PURCHASING\08 COST REDUCTION STUFF\*" | sort /+35 > %CR%
-C:\Users\kaoki\Documents\md5deep-4.4\md5deep64.exe -r "H:\PURCHASING\08 COST REDUCTION STUFF\IMPACT CALCULATOR" | sort /+35 >> %CR%
-echo ...Done
+  echo Moving along.. Digesting any updates to the cost reduction proposal estimates.
+  %md5%    "H:PURCHASING\08 COST REDUCTION STUFF\*"         |          sort /+35 > %CR%
+  %md5% -r "H:\PURCHASING\08 COST REDUCTION STUFF\IMPACT CALCULATOR" | sort /+35 >> %CR%
+  echo ...Done
 )
 
 echo[
-echo Reminder -- next step, Drag and Drop "*.sum" files from yesterday to the respective digest checkers to verify changes and track the progress.
-
+echo Reminder -- next step, Drag and Drop "*.sum" files from yesterday to the respective digests to compare changes and track the progress.
 
 pause
+goto :EOF
+
+:hashfile
+if not exist %1 (
+  echo Digesting all quotes in %2
+  %md5% -r %2 | sort /+35 > %1
+  echo ...Done
+)
+exit /b
--- side-by-side.bat
+++ side-by-side.bat
@@ -2,10 +2,12 @@
 
 echo[
 echo Highly recommend maximizing the prompt
+echo Set at 200 print columns
+echo Utilize Diffutils (Win) package
 
 echo[
 echo Compare two indices and show differences side by side
-C:\Users\kaoki\Documents\coreutils\bin\diff.exe -iEb --side-by-side --suppress-common-lines -W 390 --minimal "%~1" "%~2"
+C:\Users\kaoki\Documents\coreutils\bin\diff.exe -iEb --side-by-side --suppress-common-lines -W 200 --minimal "%~1" "%~2"
 
 
 echo[
