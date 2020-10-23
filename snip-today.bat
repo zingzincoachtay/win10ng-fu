65650015f14b1c94a164d482205f1109e13fba86
zingzincoachtay <49378571+zingzincoachtay@users.noreply.github.com>
2020-10-23 10:46:29 -0400

    Testing the retrieval of the parent directories

--- snip-today.bat
+++ snip-today.bat
@@ -1,4 +1,12 @@
+@echo off
 
-set Today=%Date:~10,4%-%Date:~4,2%-%Date:~7,2%(%Date:~0,3%)
+SET Today=%Date:~10,4%-%Date:~4,2%-%Date:~7,2%(%Date:~0,3%)
 
-echo %Today%
+ECHO %Today%
+
+SET me=%~nx0
+SET parent=%~dp0
+set boss=%~dp%parent%
+
+ECHO %me%
+ECHO %boss%
